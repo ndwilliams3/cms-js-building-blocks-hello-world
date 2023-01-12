105502eda22c9c0250c5d96a15ff142e98a6244b
Byron Matto <bmatto@hubspot.com>
2023-01-12 10:05:51 -0500

    3rd party messin

--- hello-world-project/cms-assets/components/partials/Home.jsx
+++ hello-world-project/cms-assets/components/partials/Home.jsx
@@ -1,3 +1,5 @@
+import { BeakerIcon } from '@heroicons/react/24/solid';
+
 import homeStyles from '../../styles/home.module.css';
 
 import Layout from '../Layout.jsx';
@@ -6,6 +8,8 @@ import HubSpotSprocket from '../HubSpotSprocket';
 function Home() {
   return (
     <Layout>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+
       <div className={homeStyles.logo}>
         <HubSpotSprocket link={'https://hubspot.com'} />
       </div>
