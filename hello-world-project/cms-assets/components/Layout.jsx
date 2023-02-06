3c9b50e4dd1baf59561681e18095ffc18af175d2
Timothy Finley <tfinley@hubspot.com>
2023-02-06 12:05:11 -0500

    Leave this commented

--- hello-world-project/cms-assets/components/Layout.jsx
+++ hello-world-project/cms-assets/components/Layout.jsx
@@ -1,7 +1,17 @@
+// import { useBasicSharedState } from '@hubspot/cms-components';
 import layoutStyles from '../styles/layout.module.css';
 
 function Layout({ children }) {
-  return <div className={layoutStyles.layout}>{children}</div>;
+  // const [sharedState] = useBasicSharedState();
+
+  return (
+    <div className={layoutStyles.layout}>
+      {children}
+
+      {/* <h5>Shared state on server</h5>
+      {JSON.stringify(sharedState)} */}
+    </div>
+  );
 }
 
 export default Layout;
